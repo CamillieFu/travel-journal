1e36c0dcc835d1ae8f85ce71b96f664fc94eac30
Camille Fujisaki <cae224@cornell.edu>
2022-07-01 12:30:08 +0900

    added the header component

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react"
+import globeLogo from "../images/globe.png"
+
+export default function Header() {
+  return (
+    <header>
+      <nav className="nav">
+        <img
+          src={globeLogo}
+          className="nav-logo"
+          alt="globe logo"
+        />
+        <h1 className="nav-header">
+          Camille Fujisaki's Terribly Interesting Travel Adventures
+        </h1>
+      </nav>
+    </header>
+  )
+}
