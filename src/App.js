454cd4ae3b87efcc29a41d8f9d2582597f9b3d9a
Camille Fujisaki <cae224@cornell.edu>
2022-07-12 13:15:29 +0900

    added files for main component and cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import './css/App.css';
+import Card from "./components/Card"
 import Header from './components/Header';
+import data from './data'
+// import MainComponent from './components/MainComponent';
 
 function App() {
+  const cards = data.map(item => {
+    return (
+      <Card
+        key={item.id}
+        {...item}
+
+      />
+    )
+  })
+
   return (
     <div className="App">
         <Header />
+        {/* <MainComponent /> */}
+        {cards}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
