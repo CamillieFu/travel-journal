9add8177834c02393031c668eecc0b861c119230
Camille Fujisaki <cae224@cornell.edu>
2022-07-15 14:53:43 +0900

    updated google maps links to actual links in data file

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ const data = [
     description: "Rising to 12,388 feet (3,776 metres), Mount Fuji is the tallest mountain in Japan and is known for its graceful conical form. It is the country's sacred symbol, and temples and shrines are located around and on the volcano. You should try to go on a clear day, though. Otherwise, you might not be seeing very much. ",
     date: "March 2022",
     img: "https://thumbnails.production.thenounproject.com/qzr22wYYKM5K-Dt6xdTTUY722Fw=/fit-in/1000x1000/photos.production.thenounproject.com/photos/arakurayama_sengen_park_near_fuji_mountain_in_japan-scopio-0a5f903a-bff7-49ed-89_Uld5TVo.jpeg",
-    googleMapsLink: "www.google.com",
+    googleMapsLink: "https://www.google.com/maps/place/Mount+Fuji/@35.360625,138.7273634,15z/data=!3m1!4b1!4m5!3m4!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!5m1!1e4",
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ const data = [
     description: "Florence was founded as a Roman military colony about the 1st century bce, and during its long history it has been a republic, a seat of the duchy of Tuscany, and a capital (1865–70) of Italy. During the 14th–16th century Florence achieved preeminence in commerce and finance, learning, and especially the arts. The architecture is rivaled only by the food.",
     date: "March 2022",
     img: "https://thumbnails.production.thenounproject.com/OAROaJz-sbcGUmJR6z5PTa1KrQs=/fit-in/1000x1000/photos.production.thenounproject.com/photos/aerial_view_of_city_of_florence_during_night_time-scopio-5bad1cfa-b9b0-42bd-a814-_QxIV0yU.jpg",
-    googleMapsLink: "www.google.com",
+    googleMapsLink: "https://www.google.com/maps/place/Florence,+Metropolitan+City+of+Florence,+Italy/@43.7800606,11.1707559,12z/data=!3m1!4b1!4m5!3m4!1s0x132a56a680d2d6ad:0x93d57917efc72a03!8m2!3d43.7695604!4d11.2558136!5m1!1e4",
   },
   {
     id: 3,
@@ -24,7 +24,7 @@ const data = [
     description: "Dominica is a mountainous Caribbean island nation with natural hot springs and tropical rainforests. Morne Trois Pitons National Park is home to the volcanically heated, steam-covered Boiling Lake. The park also encompasses sulphur vents, the 65m-tall Trafalgar Falls and narrow Titou Gorge. To the west is Dominica’s capital, Roseau, with colorful timber houses and botanic gardens.",
     date: "March 2022",
     img: "https://thumbnails.production.thenounproject.com/3lcO1jwCAp-FyGB-S5Qwl9SpaBA=/fit-in/1000x1000/photos.production.thenounproject.com/photos/85D903B8-DA54-410C-A406-2B56E8572159.jpg",
-    googleMapsLink: "www.google.com",
+    googleMapsLink: "https://www.google.com/maps/place/Soufriere+Bay/@13.847244,-61.084718,14z/data=!3m1!4b1!4m5!3m4!1s0x8c4089747cb23ab5:0x92e09f30c03cf6a6!8m2!3d13.8492638!4d-61.0675147!5m1!1e4",
   }
 ]
 
